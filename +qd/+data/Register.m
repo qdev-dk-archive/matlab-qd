7e8a42fd6d4170ec9df9d15c7ace274bb8443202
Anders Jellinggaard <anders.jel@gmail.com>
2013-04-29 21:11:59 -0700

    Register class.

---
+++ +qd/+data/Register.m
@@ -0,0 +1,10 @@
+classdef Register < handle
+    properties(Access=private)
+        content = struct()
+    end
+    methods
+        function add(obj, namespace, value)
+            
+        end
+    end
+end
