66439c178afbdb414781f5735a439fd0c9ce597b
triton3 <triton3@qdev.dk>
2013-05-06 10:34:41 +0200

    Fixed bugs

--- +qd/+util/class_name.m
+++ +qd/+util/class_name.m
@@ -1,10 +1,9 @@
 function r = class_name(obj, varargin)
     p = inputParser();
-    p.addOptional('full', []);
+    p.addOptional('full', [], @(x)strcmp(x, 'full'));
     p.parse(varargin{:});
     m = metaclass(obj);
     if ~isempty(p.Results.full)
-        qd.util.assert(strcmp(p.Results.full, 'full'));
         r = m.Name;
     else
         p = qd.util.strsplit(m.Name, '.');
