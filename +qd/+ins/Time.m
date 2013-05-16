3aa676d5ba1e6858309649328fb7c73c16029a2b
Merlin <merlin@nbi.dk>
2013-05-16 19:46:47 +0200

    Added Time instrument

---
+++ +qd/+ins/Time.m
@@ -0,0 +1,49 @@
+classdef Time < qd.classes.ComInstrument
+    properties(Access=private)
+        start_time;
+    end
+    
+    methods
+        function r = model(obj)
+            r = 'Time';
+        end
+
+        function r = channels(obj)
+            r = {'time'};
+        end
+        
+        function reset(obj)
+            obj.start_time = [];
+        end
+        
+        function begin(obj)
+            obj.start_time = tic;
+        end
+                  
+        function setc(obj, channel, val)
+            switch channel
+                case 'time'
+                    if isempty(obj.start_time)
+                        obj.begin();
+                    end
+                    wait_time = max(0, val - toc(obj.start_time));
+                    pause(wait_time)
+                otherwise
+                    error('not supported.')
+            end
+        end
+        
+        function val = getc(obj, channel)
+            switch channel
+                case 'time'
+                    if isempty(obj.start_time)
+                        obj.begin();
+                    end
+                    val = toc(obj.start_time);
+                otherwise
+                    error('not supported.')
+            end
+        end
+        
+    end
+end
