a6c56bc3996bf91389ab52d8af115d7cce150812
Anders Jellinggaard <anders.jel@gmail.com>
2013-05-16 11:42:40 +0200

    Possible edge case.

--- +qd/+data/view_table.m
+++ +qd/+data/view_table.m
@@ -2,7 +2,7 @@ function view = view_table(table)
     view = struct();
     view.map = containers.Map();
     for column = table
-        if isvarname(column{1}.name)
+        if isvarname(column{1}.name) && ~strcmp(column{1}.name, 'map')
             view.(column{1}.name) = column{1}.data;
         end
         view.map(column{1}.name) = column{1}.data;
