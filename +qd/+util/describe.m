31a126bb76e3cad243fbda5a9bca8077fab69b25
Anders Jellinggaard <anders.jel@gmail.com>
2014-01-27 13:54:47 +0100

    Made qd.util.describe smarter.

--- +qd/+util/describe.m
+++ +qd/+util/describe.m
@@ -3,11 +3,29 @@
 % describing the object. For instance, when describing a channel, the
 % associated instrument is placed in the register.
 function describe(obj)
+    m = metaclass(obj);
+    for x = (m.MethodList).'
+        if strcmp(x.Name, 'describe')
+            if length(x.InputNames) == 1
+                % It only takes obj. Do describeSimpl
+                describeSimpl(obj);
+                return;
+            end
+        end
+    end
+    describeReg(obj);
+end
+
+function describeSimpl(obj)
+    disp(json.dump(obj.describe(), 'indent', 2));
+end
+
+function describeReg(obj)
     r = qd.classes.Register;
     d = obj.describe(r);
-    disp('Register:')
-    disp(json.dump(r.describe(), 'indent', 2)) 
-    disp(' ')
-    disp('Object:')
-    disp(json.dump(d, 'indent', 2)) 
+    disp('Register:');
+    disp(json.dump(r.describe(), 'indent', 2));
+    disp(' ');
+    disp('Object:');
+    disp(json.dump(d, 'indent', 2));
 end
