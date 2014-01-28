72affee3bff2bb789549840f3b8b8146b7fcc6ba
Anders Jellinggaard <anders.jel@gmail.com>
2014-01-28 12:35:45 +0100

    Added breakdown run.

---
+++ +qd/+run/BreakdownRun.m
@@ -0,0 +1,25 @@
+classdef BreakdownRun < qd.run.StandardRun
+    properties
+        % breakdown_condition should be set to a funtion handle taking a
+        % single parameter 'inputs', which is a containers.Map mapping the
+        % name of each input to the last meassured value. If the function
+        % returns true, then the move_to_zero is called, and the run
+        % terminates.
+        breakdown_condition = @(inputs) false;
+    end
+
+    methods(Access=protected)
+
+        function row_hook(obj, sweep_values, inputs)
+            input_map = container.Map();
+            for i = [1..length(inputs)]
+                input_map(obj.inputs{i}.name) = inputs(i);
+            end
+            if obj.breakdown_condition(input_map)
+                obj.move_to_zero();
+                throw(MException('qd:run:BreakdownRun:breakdown', 'breakdown condition met.'))
+            end
+        end
+
+    end
+end
