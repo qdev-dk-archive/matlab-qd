8796fffeb4fdf8a2338ee7118e63609c53431994
Triton 4 <e@mail.com>
2014-04-21 16:08:47 +0200

    Guen - SafeRun
    Run that has a dialog to pause/stop a measurement
    also has live plotting functionality (under construction)

--- +qd/+run/SafeRun.m
+++ +qd/+run/SafeRun.m
@@ -2,12 +2,16 @@ classdef SafeRun < qd.run.StandardRun
     properties
         running = false;
         stopnow = false;
+        plots = {} %Cell array containing plots
+    end
+    properties(Access=private)
+        columns
     end
     methods
-    	function pause_run(obj)
-    		disp('Run will pause.');
-			obj.running = false;
-    	end
+        function pause_run(obj)
+            disp('Run will pause.');
+            obj.running = false;
+        end
 
         function continue_run(obj)
             disp('Run continued.');
@@ -18,29 +22,101 @@ classdef SafeRun < qd.run.StandardRun
             disp('Run stopped.');
             obj.stopnow = true;
         end
+
+        function add_plot(obj, xname, yname, fignum, varargin)
+            p = containers.Map;
+            p('xname') = xname;
+            p('yname') = yname;
+            p('varargin') = varargin;
+            p('fignum') = fignum;
+            obj.plots{end+1} = p;
+        end
+
+        function create_plots(obj)
+            for pnum = 1:length(obj.plots)
+                h = figure(obj.plots{pnum}('fignum'));
+                clf();
+                obj.plots{pnum}('handle') = h;
+                xname = obj.plots{pnum}('xname');
+                yname = obj.plots{pnum}('yname');
+                xlabel(xname);
+                ylabel(yname);
+                title(strrep(obj.store.name,'_','\_'));
+            end
+        end
+
+        function update_plots(obj, values)
+            for p = obj.plots
+                h = p{1}('handle');
+                xname = p{1}('xname');
+                yname = p{1}('yname');
+                varargin = p{1}('varargin');
+                xindex = not(cellfun('isempty', strfind(obj.columns, xname)));
+                yindex = not(cellfun('isempty', strfind(obj.columns, yname)));
+                figure(h); hold on;
+                x = values(xindex);
+                y = values(yindex);
+                plot(x, y, varargin{:});
+                mytitle = [obj.store.datestamp, '/', obj.store.timestamp, ' ', strrep(obj.store.name,'_','\_')];
+                title(mytitle);
+                xlabel(xname);
+                ylabel(yname);
+            end
+        end
+
+        function obj = sweep(obj, name_or_channel, from, to, points, varargin)
+            p = inputParser();
+            p.addOptional('settle', 0);
+            p.addOptional('tolerance', []);
+            p.addOptional('values', []);
+            p.addOptional('alternate', false);
+            p.parse(varargin{:});
+            sweep = struct();
+            sweep.from = from;
+            sweep.to = to;
+            sweep.points = points;
+            sweep.settle = p.Results.settle;
+            sweep.tolerance = p.Results.tolerance;
+            sweep.alternate = p.Results.alternate;
+            if(isempty(p.Results.values))
+                sweep.values = linspace(from, to, points);
+            else
+                sweep.values = p.Results.values;
+            end
+            sweep.chan = obj.resolve_channel(name_or_channel);
+            if(strcmp(name_or_channel,'time/time') && (sweep.from == 0))
+                sweep.chan.instrument.reset;
+            end
+            obj.sweeps{end + 1} = sweep;
+        end
     end
 
-	methods(Access=protected)
-		function perform_run(obj, out_dir)
+    methods(Access=protected)
+        function perform_run(obj, out_dir)
             % This table will hold the data collected.
             table = qd.data.TableWriter(out_dir, 'data');
+            obj.columns = {};
             for sweep = obj.sweeps
                 table.add_channel_column(sweep{1}.chan);
+                obj.columns{end+1} = sweep{1}.chan.name;
             end
             for inp = obj.inputs
                 table.add_channel_column(inp{1});
+                obj.columns{end+1} = inp{1}.name;
             end
             table.init();
-        	obj.running = true;
+            obj.running = true;
             obj.stopnow = false;
             % Start meas control window.
             hMeasControl = meas_control(obj);
+            % Create plots
+            obj.create_plots();
             % Now perform all the measurements.
-            obj.handle_sweeps(obj.sweeps, [], obj.initial_settle, table);
+            obj.handle_sweeps(obj.sweeps, [], table);
             close(hMeasControl);
         end
 
-        function handle_sweeps(obj, sweeps, earlier_values, settle, table)
+        function handle_sweeps(obj, sweeps, earlier_values, table)
         % obj.handle_sweeps(sweeps, earlier_values, settle, table)
         %
         % Sweeps the channels in sweeps, takes measurements and puts them in
@@ -56,23 +132,29 @@ classdef SafeRun < qd.run.StandardRun
             % If there are no more sweeps left, let the system settle, then
             % measure one point.
             if isempty(sweeps)
-                if(settle > 0)
-                    pause(settle);
-                end
-                values = [earlier_values];
+                %values = [earlier_values];
+                values = [];
                 futures = {};
+                for sweep = obj.sweeps
+                    futures{end + 1} = sweep{1}.chan.get_async();
+                end
                 for inp = obj.inputs
                     futures{end + 1} = inp{1}.get_async();
                 end
+                % for sweep = obj.sweeps
+                %     values(end + 1) = earlier_values(end);
+                % end
                 for future = futures
                     values(end + 1) = future{1}.exec();
                 end
-                table.add_point(values);
+                % Add data point
+                table.add_point(values); % Write data point to file
+                obj.update_plots(values);
                 drawnow();
                 if obj.running
-                	return
+                    return
                 else
-                	disp('Click continue.');
+                    disp('Click continue.');
                     while (not(obj.running) && not(obj.stopnow))
                         pause(1);
                     end
@@ -84,16 +166,24 @@ classdef SafeRun < qd.run.StandardRun
             % function with one less channel to sweep.
             sweep = sweeps{1};
             next_sweeps = sweeps(2:end);
-            for value = linspace(sweep.from, sweep.to, sweep.points)
+            for value = sweep.values
                 sweep.chan.set(value);
-                settle = max(settle, sweep.settle);
-                obj.handle_sweeps(next_sweeps, [earlier_values value], settle, table);
-                % In the first iteration of the loop, we need to wait for the
-                % previously changed value to settle. We also need to wait for
-                % this value to settle, whichever is greater. In the next
-                % iteration of the loop, we only need to wait for this value,
-                % therefore settle is set to 0 here.
-                settle = 0;
+                if ~isempty(sweep.tolerance)
+                    curval = sweep.chan.get();
+                    fprintf('Setting %s=%f to %f\r',sweep.chan.name,curval,value);
+                    while true
+                        curval = sweep.chan.get();
+                        if abs(value-curval)<sweep.tolerance
+                            break;
+                        else
+                            pause(sweep.settle);
+                        end
+                    end
+                else
+                    pause(sweep.settle);
+                end
+                %settle = max(settle, sweep.settle);
+                obj.handle_sweeps(next_sweeps, [earlier_values value], table);
                 if ~isempty(next_sweeps)
                     % Nicely seperate everything for gnuplot.
                     table.add_divider();
@@ -104,5 +194,5 @@ classdef SafeRun < qd.run.StandardRun
                 end
             end
         end
-	end
+    end
 end
