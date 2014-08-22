13cfdf6bd61b393a222f23add00f6bc9fc6a442e
Anders Jellinggaard <anders.jel@gmail.com>
2014-08-22 13:14:58 +0200

    Rhode and Schwarz  SMF100A driver

---
+++ +qd/+ins/RSSMF100A.m
@@ -0,0 +1,48 @@
+classdef RSSMF100A < qd.classes.ComInstrument
+    methods
+        function obj = RSSMF100A(varargin)
+            obj@qd.classes.ComInstrument(varargin{:});
+        end
+
+        function cs = channels(obj)
+            cs = {'freq', 'pow'};
+        end
+
+        function setc(obj, chan, val)
+            switch chan
+                case 'freq'
+                    obj.sendf('FREQ %.16f', val);
+                case 'pow'
+                    obj.sendf('POW %.16f', val);
+                otherwise
+                    error('No such channel');
+            end
+        end
+
+        function val = getc(obj, chan)
+            switch chan
+                case 'freq'
+                    val = obj.querym('FREQ', '%f');
+                case 'pow'
+                    val = obj.querym('POW', '%f');
+                otherwise
+                    error('No such channel');
+            end
+        end
+
+        function turn_on(obj)
+            obj.send('OUTP 1')
+        end
+
+        function turn_off(obj)
+            obj.send('OUTP 0')
+        end
+
+        function r = describe(obj, register)
+            r = obj.describe@qd.classes.ComInstrument(register);
+            r.freq = obj.getc('freq');
+            r.pow = obj.getc('pow');
+            r.is_on = obj.querym('OUTP?', '%d');
+        end
+    end
+end
