5f698870606bb8240e8ad494149b7250516438d0
Anders Jellinggaard <anders.jel@gmail.com>
2014-09-02 08:55:45 +0200

    Nicer format seconds

--- +qd/+util/format_seconds.m
+++ +qd/+util/format_seconds.m
@@ -1,25 +1,25 @@
-function string = format_seconds(seconds_total)
-    seconds_total = round(seconds_total);
-    minutes_total = floor(seconds_total / 60);
-    hours_total = floor(minutes_total / 60);
-    days_total = floor(hours_total / 24);
-    seconds = floor(mod(seconds_total, 60));
-    minutes = mod(minutes_total, 60);
-    hours = mod(hours_total, 24);
-    started_printing = false;
-    string = '';
-    if days_total > 0
-        string = sprintf('%s %d days', string, days_total);
-        started_printing = true;
+function s = format_seconds(seconds)
+    seconds = round(seconds);
+    minutes = floor(seconds/60);
+    seconds = mod(seconds, 60);
+    hours = floor(minutes/60);
+    minutes = mod(minutes, 60);
+    days = floor(hours/24);
+    hours = mod(hours, 24);
+
+    parts = {};
+    if days > 0
+        parts{end + 1} = sprintf('%d days', days);
     end
-    if hours > 0 || started_printing
-        string = sprintf('%s %d h', string, hours);
-        started_printing = true;
+    if hours > 0 || ~isempty(parts)
+        parts{end + 1} = sprintf('%d h', hours);
     end
-    if minutes > 0 || started_printing
-        string = sprintf('%s %d min', string, minutes);
+    if minutes > 0 || ~isempty(parts)
+        parts{end + 1} = sprintf('%d min', minutes);
     end
-    if hours_total < 1
-        string = sprintf('%s %d s', string, seconds);
+    if days < 1 && hours < 1
+        parts{end + 1} = sprintf('%d s', seconds);
     end
+
+    s = qd.util.strjoin(parts, ' ');
 end
