487af80b794888f5057626a873ad9250f3f34369
Anders Jellinggaard <anders.jel@gmail.com>
2014-09-26 14:44:58 +0200

    Missing semicolon.

--- +qd/+util/set.m
+++ +qd/+util/set.m
@@ -2,7 +2,7 @@ function set(varargin)
     % qd.util.set(chan, val, [chan, val, ...])
     %
     % Set several channels in parallel.
-    f = []
+    f = [];
     for i = 1:2:length(varargin)
         f = f & varargin{i}.set_async(varargin{i + 1});
     end
