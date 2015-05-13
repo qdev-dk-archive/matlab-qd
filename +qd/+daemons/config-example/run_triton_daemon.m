83b3c672524d97bf89693857984428d176fff969
Anders Jellinggaard <anders.jel@gmail.com>
2015-05-13 09:24:29 +0200

    BREAKING. Changed API of Triton instrument
    See BREAKING_CHANGES.md.

--- +qd/+daemons/config-example/run_triton_daemon.m
+++ +qd/+daemons/config-example/run_triton_daemon.m
@@ -1,10 +1,13 @@
 function run_triton_daemon()
     json.startup();
-    qd.util.change_matlab_title('Triton Daemon');s.Triton();
-    triton = qd.daemon
+    qd.util.change_matlab_title('Triton Daemon');
+    triton = qd.daemon.Triton();
     triton.address = '172.20.???.???';
     triton.password = '?????';
     % triton.server.smtp_server = 'mail.fys.ku.dk';
     % triton.server.alert_email = 'email@example.com';
+
+    % You can configure extra temperature channels here
+    % triton.channels('MC_cernox') = 'T5'
     triton.run_daemon();
 end
